3641898aaa60a6646964cf221537e30d16153f6a
Piotr Polański <ppolanski@pgs-soft.com>
2017-05-24 11:09:03 +0200

    uml classes extractor refactoring

---
+++ EyeOfBeholder.Uml.Tests/ClassesExtractorTests.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Linq;
+using EyeOfBeholder.Uml.UmlType;
+using Xunit;
+
+namespace EyeOfBeholder.Uml.Tests
+{
+    public class ClassesExtractorTests
+    {
+        [Fact]
+        public void GetFrom_Given_validCSharpCode_Return_UmlClasses()
+        {
+            //arrange
+            var classesExtractor= new ClassesExtractor();
+            var codeString = File.ReadAllText(@"Code\SimpleClass.cs");
+
+            //act
+            var umlClasses = classesExtractor.GetFrom(codeString);
+
+            //assert
+            Assert.NotEmpty(umlClasses);
+        }
+
+        [Fact]
+        public void GetFrom_Given_SimpleClass_Return_UmlClassRepresentation()
+        {
+            //arrange
+            var classesExtractor= new ClassesExtractor();
+            var codeString = File.ReadAllText(@"Code\SimpleClass.cs");
+
+            //act
+            var umlClasses = classesExtractor.GetFrom(codeString);
+
+            //assert
+            Assert.Equal(3, umlClasses.Count());
+            var simpleClass = umlClasses.First();
+            Assert.Equal("SimpleClass", simpleClass.Name);
+
+            Assert.Equal(2, simpleClass.Attributes.Count);
+            var firstAttribute = simpleClass.Attributes.First();
+            Assert.Equal("name", firstAttribute.Name);
+            Assert.Equal("SomeBaseType", firstAttribute.TypeName);
+            Assert.Equal(VisibilityType.Private, firstAttribute.VisibilityType);
+            var secondAttribute = simpleClass.Attributes.Last();
+            Assert.Equal("Number", secondAttribute.Name);
+            Assert.Equal("Int32", secondAttribute.TypeName);
+            Assert.Equal(VisibilityType.Public, secondAttribute.VisibilityType);
+
+
+            Assert.Equal(2, simpleClass.Associations.Count);
+            var firstAssociation = simpleClass.Associations.First();
+            Assert.Equal("SomeBaseType", firstAssociation.BaseTypeName);
+            Assert.Equal("name", firstAssociation.AttributeName);
+            Assert.Equal(UmlClassType.Class, firstAssociation.Type);
+            var secondAssociation = simpleClass.Associations.Last();
+            Assert.Equal("Number", secondAssociation.AttributeName);
+            Assert.Equal("Int32", secondAssociation.BaseTypeName);
+            Assert.Equal(UmlClassType.Class, secondAssociation.Type);
+
+            Assert.Equal(1, simpleClass.Dependencies.Count);
+            var firstDependency = simpleClass.Dependencies.First();
+            Assert.Equal("SomeBaseType", firstDependency.ClassName);
+            Assert.Equal("newName", firstDependency.RelationName);
+            Assert.Equal(UmlClassType.Class, firstDependency.Type);
+
+            Assert.Equal(1, simpleClass.Operations.Count);
+            var firstOperation = simpleClass.Operations.First();
+            Assert.Equal("SomeMethod", firstOperation.Name);
+            Assert.Equal("Boolean", firstOperation.ReturnTypeName);
+            Assert.Equal(VisibilityType.Public, firstOperation.VisibilityType);
+
+            Assert.Equal(1, simpleClass.Realizations.Count);
+            var firstRealization = simpleClass.Realizations.First();
+            Assert.Equal("ISomeInterface", firstRealization.Name);
+            Assert.Equal(UmlClassType.Interface, firstRealization.Type);
+
+            Assert.Equal("SomeBaseType", simpleClass.SuperClass.Name);
+            Assert.Equal(UmlClassType.Class, simpleClass.Type);
+            Assert.Equal(VisibilityType.Public, simpleClass.VisibilityType);
+
+            var secondClass = umlClasses.Skip(1).First();
+            Assert.Equal("SomeBaseType", secondClass.Name);
+            Assert.Empty(secondClass.Associations);
+            Assert.Empty(secondClass.Attributes);
+            Assert.Empty(secondClass.Dependencies);
+            Assert.Empty(secondClass.Operations);
+            Assert.Empty(secondClass.Realizations);
+            Assert.Null(secondClass.SuperClass);
+
+            var @interface = umlClasses.Skip(2).First();
+            Assert.Equal("ISomeInterface", @interface.Name);
+            Assert.Empty(@interface.Associations);
+            Assert.Empty(@interface.Attributes);
+            Assert.Empty(@interface.Dependencies);
+            Assert.Empty(@interface.Operations);
+            Assert.Empty(@interface.Realizations);
+            Assert.Null(@interface.SuperClass);
+
+        }
+    }
+}
---
+++ EyeOfBeholder.Uml/ClassesExtractor.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EyeOfBeholder.Uml.UmlType;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EyeOfBeholder.Uml
+{
+    public class ClassesExtractor
+    {
+        public IEnumerable<UmlClass> GetFrom(string codeString)
+        {
+            var classes = new List<UmlClass>();
+            var tree = CSharpSyntaxTree.ParseText(codeString);
+            tree.GetRoot();
+            var syntaxRoot = (CompilationUnitSyntax)tree.GetRoot();
+
+            var compilation = CSharpCompilation.Create("HelloWorld")
+                .AddReferences(
+                    MetadataReference.CreateFromFile(
+                        typeof(object).Assembly.Location))
+                .AddSyntaxTrees(tree);
+
+            var model = compilation.GetSemanticModel(tree);
+
+            var myClasses = syntaxRoot.DescendantNodes().OfType<ClassDeclarationSyntax>();
+
+
+
+            foreach (var classDeclarationSyntax in myClasses)
+            {
+                var classSymbol = model.GetDeclaredSymbol(classDeclarationSyntax);
+                var visibility = classSymbol.DeclaredAccessibility;
+                var visibilityType = GetVisibilityType(visibility);
+                var name = classDeclarationSyntax.Identifier.ValueText;
+
+                var attributes = new List<Attribute>();
+                var associations = new List<Association>();
+                AddAttributesAndAssociations(classSymbol, attributes, associations);
+
+                var realizations = new List<Realization>();
+                AddRealizations(classSymbol, realizations);
+
+                var operations = new List<Operation>();
+                AddOperations(classSymbol, operations);
+
+                SuperClass superClass = null;
+                superClass = SetSuperClass(classSymbol, superClass);
+
+
+                var deps = new List<Dependency>();
+                deps = GetDependecies(classSymbol, deps);
+
+                var umlClass = new UmlClass(
+                    name,
+                    visibilityType,
+                    associations,
+                    attributes,
+                    realizations,
+                    superClass,
+                    deps,
+                    operations
+                    );
+
+                classes.Add(umlClass);
+            }
+
+            var @interfaces = syntaxRoot.DescendantNodes().OfType<InterfaceDeclarationSyntax>();
+            foreach (var @interface in @interfaces)
+            {
+                var @interfaceSymbol = model.GetDeclaredSymbol(@interface);
+                var visibility = @interfaceSymbol.DeclaredAccessibility;
+                var visibilityType = GetVisibilityType(visibility);
+                var name = @interface.Identifier.ValueText;
+
+                var attributes = new List<Attribute>();
+                var associations = new List<Association>();
+                AddAttributesAndAssociations(@interfaceSymbol, attributes, associations);
+
+                var realizations = new List<Realization>();
+                AddRealizations(@interfaceSymbol, realizations);
+
+                var operations = new List<Operation>();
+                AddOperations(@interfaceSymbol, operations);
+
+                SuperClass superClass = null;
+                superClass = SetSuperClass(@interfaceSymbol, superClass);
+
+
+                var deps = new List<Dependency>();
+                deps = GetDependecies(@interfaceSymbol, deps);
+
+                var umlClass = new UmlClass(
+                    name,
+                    visibilityType,
+                    associations,
+                    attributes,
+                    realizations,
+                    superClass,
+                    deps,
+                    operations
+                    );
+
+                classes.Add(umlClass);
+            }
+
+            return classes;
+        }
+
+        private static List<Dependency> GetDependecies(INamedTypeSymbol classSymbol, List<Dependency> deps)
+        {
+            var ctors = classSymbol.Constructors;
+            var ctorArgumentsDistinct = new Dictionary<string, Dependency>();
+            foreach (var ctor in ctors)
+            {
+                foreach (var parameter in ctor.Parameters)
+                {
+                    if (!ctorArgumentsDistinct.ContainsKey(parameter.Name) &&
+                        !parameter.Type.ContainingNamespace.Name.StartsWith("System"))
+                    {
+                        var depName = parameter.Name;
+                        var depTypeName = parameter.Type.Name;
+                        var depType = UmlClassType.Class;
+                        if (parameter.Type.IsReferenceType)
+                        {
+                            depType = UmlClassType.Class;
+                        }
+
+                        ctorArgumentsDistinct[parameter.Name] = new Dependency(depTypeName, depType, depName);
+                    }
+                }
+            }
+
+            deps = ctorArgumentsDistinct.Select(a => a.Value).ToList();
+            return deps;
+        }
+
+        private SuperClass SetSuperClass(INamedTypeSymbol classSymbol, SuperClass superClass)
+        {
+            if (classSymbol.BaseType != null && !classSymbol.BaseType.ContainingNamespace.Name.StartsWith("System"))
+            {
+                var baseClassName1 = classSymbol.BaseType.Name;
+                var superClassType = classSymbol.BaseType.TypeKind;
+
+                superClass = new SuperClass(baseClassName1, GetUmlClassType(superClassType));
+            }
+            return superClass;
+        }
+
+        private void AddOperations(INamedTypeSymbol classSymbol, List<Operation> operations)
+        {
+            var methods = classSymbol.GetMembers().OfType<IMethodSymbol>()
+                .Where(m => m.MethodKind != MethodKind.Constructor
+                            && m.MethodKind != MethodKind.PropertyGet && m.MethodKind != MethodKind.PropertySet);
+            foreach (var method in methods)
+            {
+                //var op = model.GetDeclaredSymbol(method);
+                var opname = method.Name;
+                var opVisibility = method.DeclaredAccessibility;
+                var returnType = method.ReturnType.Name;
+                    //rawOperation.DescendantNodes().OfType<PredefinedTypeSyntax>().First().Keyword.ValueText;
+                var opvisibility = GetVisibilityType(opVisibility);
+                var operation = new Operation(opname, returnType, opvisibility);
+                operations.Add(operation);
+            }
+        }
+
+        private static void AddRealizations(INamedTypeSymbol classSymbol, List<Realization> realizations)
+        {
+            foreach (var @interface in classSymbol.Interfaces)
+            {
+                var realizationName = @interface.Name;
+                realizations.Add(new Realization(realizationName, UmlClassType.Interface));
+            }
+        }
+
+        private void AddAttributesAndAssociations(INamedTypeSymbol classSymbol, List<Attribute> attributes, List<Association> associations)
+        {
+            var fids = classSymbol.GetMembers().OfType<IFieldSymbol>().Where(f => !f.IsImplicitlyDeclared);
+            foreach (var field in fids)
+            {
+                //var f = model.GetDeclaredSymbol(field);
+                var attributeName = field.Name;
+                var attributeType = field.Type.Name; //.Declaration.DescendantNodes().OfType<IdentifierNameSyntax>().First()
+                var attributeVisibility = field.DeclaredAccessibility;
+                var vis = GetVisibilityType(attributeVisibility);
+                var attribute = new Attribute(attributeName, attributeType, vis);
+                attributes.Add(attribute);
+
+                var association = new Association(attributeName, attributeType, UmlClassType.Class);
+                associations.Add(association);
+            }
+
+            var props = classSymbol.GetMembers().OfType<IPropertySymbol>();
+            foreach (var property in props)
+            {
+                //var pr = model.GetDeclaredSymbol(property);
+                var propertyName = property.Name;
+                var propertyType = property.Type.Name;
+                var propVisibility = property.DeclaredAccessibility;
+                var visType = GetVisibilityType(propVisibility);
+                var prop = new Attribute(propertyName, propertyType, visType);
+                attributes.Add(prop);
+
+                var propAss = new Association(propertyName, propertyType, UmlClassType.Class);
+                associations.Add(propAss);
+            }
+        }
+
+        private UmlClassType GetUmlClassType(TypeKind typeKind)
+        {
+            return UmlClassType.Class;
+        }
+
+        private VisibilityType GetVisibilityType(Accessibility visibility)
+        {
+            switch (visibility)
+            {
+                case Accessibility.Private:
+                    return VisibilityType.Private;
+                case Accessibility.Protected:
+                    return VisibilityType.Protected;
+                case Accessibility.Internal:
+                    return VisibilityType.Package;
+                default: return VisibilityType.Public;
+            }
+        }
+    }
+}
